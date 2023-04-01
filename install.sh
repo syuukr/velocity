f1336ef309df63387cd745e06bc93d659b404048
Siel <sielpersonal@icloud.com>
2023-04-01 14:53:48 +0900

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this as root"
+  exit
+fi
+
+modprobe ip_conntrack
+
+# Apply sysctl
+cp ./sysctl.conf /etc
+sysctl -p
+
+cp ./thewar/thewar.service /etc/systemd/system
+cp ./thepvp/thepvp.service /etc/systemd/system
+
+systemctl daemon-reload
+systemctl restart thewar
+systemctl restart thepvp
+
+
+echo "Done."
+
+exit 0;
